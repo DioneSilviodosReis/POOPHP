930c1b71ab6bda1308ffbf8b44669847ca40e429
DioneSilviodosReis <100045795+DioneSilviodosReis@users.noreply.github.com>
2024-02-19 22:26:55 -0300

    aula 01 02 03 POOPHP

---
+++ aula02/Caneta.php
@@ -0,0 +1,23 @@
+<?php
+class Caneta {
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampada;
+
+    function rabiscar(){
+        if($this->tampada == true){
+            echo "Estou tampada.....<br>";
+        }
+        else{
+        echo "Estou rabiscando.....";
+        }
+    }
+    function tampar(){
+        $this->tampada = true;
+    }
+    function destampar(){
+        $this->tampada = false;
+    }
+}
